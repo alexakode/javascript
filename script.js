a82d23ad3780d9518b3edfaab2e42d160ffde066
Alexander <alexanderm.kodehode@gmail.com>
2025-06-30 12:11:01 +0200

    functions and hoisting

---
+++ script.js
@@ -0,0 +1,13 @@
+//Lesson 4: Functions in JavaScript
+
+//Section 1: Function Declarations and hoisting
+//
+
+function sayHello() {
+  console.log("Hello");
+}
+
+sayHello();
+sayHello();
+sayHello();
+sayHello();
