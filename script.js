3d8b2a029bc464c6179b6670b88434fcb9e52c61
Alexander <alexanderm.kodehode@gmail.com>
2025-07-01 13:14:11 +0200

    new js feature: tosplice

--- script.js
+++ script.js
@@ -24,3 +24,23 @@ console.log(Number(numericString)); // converts string to number; returns NaN if
 console.log(parseInt(numericString)); //parses as an integer until an invalid character
 
 console.log(parseFloat(numericString)); // parses as a float until an invalid character; returns 123.45
+
+// Section 2: Array methods
+
+const colors = ["Red", "Green", "Blue"];
+console.log(colors.length); //number of elements
+colors.push("Yellow"); //adds an element to the array
+console.log(colors);
+console.log(colors.length);
+const lastColor = colors.pop(); //.pop removes and returns the last element, we can assign that to its own const
+console.log(lastColor);
+console.log(colors);
+console.log(colors.join(" ")); //opposite of split, joins an array into a string
+console.log(colors.includes("Green"));
+console.log(colors.shift()); //.shift removes and returns the first element, we can assign that to its own
+colors.unshift("Purple"); //adds to the beginning of the array
+console.log(colors);
+
+const newColors = colors.toSpliced(1, 1, "Orange");
+console.log(colors)
+console.log(newColors)
