d7ccce04f6f0c4ed1a4e277a635e566663b1ed76
Alexander <alexanderm.kodehode@gmail.com>
2025-07-01 12:51:11 +0200

    strings/numbers

--- script.js
+++ script.js
@@ -15,3 +15,12 @@ console.log(trimmedText.charAt(0));
 console.log(trimmedText.length); // returns the length of the string
 console.log(trimmedText.split(" ")); //splits the sting into array of strings by the character, space makes the most sense
 console.log(trimmedText.repeat(3));
+
+//1.2: converting a string to a number
+
+const numericString = "123.45xyz";
+console.log(Number(numericString)); // converts string to number; returns NaN if any part is invalid
+
+console.log(parseInt(numericString)); //parses as an integer until an invalid character
+
+console.log(parseFloat(numericString)); // parses as a float until an invalid character; returns 123.45
