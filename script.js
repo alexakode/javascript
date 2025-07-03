0945e2339a7a6aa66bb9e3b340757f633351b490
Alexander <alexanderm.kodehode@gmail.com>
2025-07-03 12:18:53 +0200

    Section 1: Check if a list includes an item

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-// Section 1: Check if a list includes an itemm (funtion, ternary, array)
+// Section 1: Check if a list includes an item (funtion, ternary, array)
 
-const shoppingList
+const shoppingList = ["milk", "bread", "cheese"];
+const gamesList = ["mario kart", "GTA6", "hello kitty island"];
+
+function checkItem(item, itemArray) {
+  let included = "";
+  if (itemArray.includes(item)) {
+    included = "does";
+  } else {
+    included = "doesn't";
+  }
+  return `The array ${included} include ${item}`;
+}
+
+console.log(checkItem("bread", shoppingList));
+console.log(checkItem("milk", shoppingList));
+console.log(checkItem("mario kart", gamesList));
