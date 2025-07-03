b848a49fbd9e7a5f1795f42689eddc7241c4e184
Alexander <alexanderm.kodehode@gmail.com>
2025-07-03 13:10:17 +0200

    palindrome checker

--- script.js
+++ script.js
@@ -3,11 +3,27 @@
 const shoppingList = ["milk", "bread", "cheese"];
 const gamesList = ["mario kart", "GTA6", "hello kitty island"];
 
-function checkItem(item, itemArray) {
-  let included = itemArray.includes(item) ? "does" : "doesn't";
-  return `The array ${included} include ${item}`;
-}
+const checkItem = (item, itemArray) =>
+  `The array does ${itemArray.includes(item) ? "" : "n't"} include ${item}`;
 
 console.log(checkItem("bread", shoppingList));
 console.log(checkItem("milk", shoppingList));
 console.log(checkItem("mario kart", gamesList));
+
+// Section 2: Convert a sentence into kebab case (function, methods)
+
+function toKebabCase(string) {
+  return string.trim().replaceAll(" ", "-");
+}
+console.log(toKebabCase("   hello javascript how are you  "));
+
+// Section 3: Checking if a word is a palindrome (function, methods)
+function isPalindrome(word) {
+  reversedWord = word.toLowerCase().split("").toReversed().join("");
+  return `${word} is ${
+    word.toLowerCase() === reversedWord ? "" : "not "
+  }a palindrome`;
+}
+//How can we return a lowercase word with an uppercase first letter
+console.log(isPalindrome("Racecar")); // racecar is a palindrome
+console.log(isPalindrome("Banana")); // banana is not a palindrome
