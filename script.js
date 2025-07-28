65a985057bf1e3134bc346396c6ec891fae7c3fc
Alexander <alexanderm.kodehode@gmail.com>
2025-07-28 12:17:08 +0200

    push for loops

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //Loops/løkker
+
+const names = ["Joe", "Ole", "Bill", "Jane", "Mary"];
+
+// 1.
+// "Hei Joe!"
+// 2.
+console.log("Hei " + names[0] + "!");
+// 3.
+console.log(`Hei ${names[0]}!`);
+
+console.log(`Hei ${names[1]}!`);
+
+console.log(`Hei ${names[2]}!`);
+
+console.log(`Hei ${names[3]}!`);
+
+console.log(`Hei ${names[4]}!`);
+
+// 4.
+for (let i = 0; i < names.length; i++) {
+  console.log(`Hei ${names[i]}!`);
+}
