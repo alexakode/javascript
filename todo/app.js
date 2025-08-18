7586e56f5d212081a546b425744900adede7a074
Alexander <alexanderm.kodehode@gmail.com>
2025-08-18 10:21:24 +0200

    fix: correct variable naming and enhance buildPage function to render tasks

--- todo/app.js
+++ todo/app.js
@@ -1,15 +1,39 @@
-const list-container = document.getElementById('list-container');
+const listContainer = document.getElementById("list-container");
 
-let tasks = [];
+let tasks = [
+    /*
+{
+description: "Buy eggs",
+isCompleted: true,
+}
+    */ 
+];
 
 // load individual tasks, render on page
-const buildPage = () => {};
+const buildPage = (tasks) => {
+  // loop over each task, create some elements
+  tasks.forEach((task) => {
+    const taskContainer = document.createElement("div");
+    taskContainer.classList.add("task-container");
+
+    const descriptionElement = document.createElement("p");
+    descriptionElement.classList.add("description");
+    descriptionElement.textContent = task.description;
+    // {
+    // description: "buy eggs",
+    // date: "",
+    // isCompleted
+    // }
+    taskContainer.append(descriptionElement);
+    listContainer.append(taskContainer);
+  });
+};
 
 // render our page
 const renderPage = () => {
-    // load tasks from local storage
+  // load tasks from local storage
 
-    // filters a-z, z-a, newest, oldest, vice versa
+  // filters a-z, z-a, newest, oldest, vice versa
 
-    buildPage(tasks);
+  buildPage(tasks);
 };
