0b49c7a0c36d005c7f4ab778118104afddf43d01
Alexander <alexanderm.kodehode@gmail.com>
2025-08-18 09:58:08 +0200

    initialize task management functions and render logic in app.js

---
+++ todo/app.js
@@ -0,0 +1,15 @@
+const list-container = document.getElementById('list-container');
+
+let tasks = [];
+
+// load individual tasks, render on page
+const buildPage = () => {};
+
+// render our page
+const renderPage = () => {
+    // load tasks from local storage
+
+    // filters a-z, z-a, newest, oldest, vice versa
+
+    buildPage(tasks);
+};
